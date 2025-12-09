abd96dab1390c2784d95cb5616f94f33328340f4
Andres <anrodriguezbr@est.utn.ac.cr>
2025-12-08 19:36:55 -0600

    Se realiza verificacion de autorizacion en rutas especificas de ranking

--- server/routes/rankingRoutes.js
+++ server/routes/rankingRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import * as rankingServices from "../services/rankingServices.js";
 import { allowRoles } from "../middlewares/roleMiddleware.js";
+
 const router = express.Router();
 // Obtener todos los rankings
 router.get('/', async (req, res) => {
@@ -38,7 +39,7 @@ router.post('/',allowRoles('admin'), async (req, res) => {
   }
 });
 
-router.put('/:id', async (req, res) => {
+router.put('/:id',allowRoles('admin'), async (req, res) => {
   try {
     const updated = await rankingServices.update(req.params.id, req.body);
     res.json(updated);
@@ -51,7 +52,7 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id',allowRoles('admin'),async (req, res) => {
   try {
     const result = await rankingServices.deleteById(req.params.id);
     res.json(result);
