bc0bc7b7536478b7abdc44c7352ce0161cd92c15
Andres <anrodriguezbr@est.utn.ac.cr>
2025-12-08 19:39:26 -0600

    Se realiza verificacion de autorizacion en rutas especificas de retos

--- server/routes/challengesRoutes.js
+++ server/routes/challengesRoutes.js
@@ -23,7 +23,7 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id',allowRoles('admin'), async (req, res) => {
   try {
     const result = await challengesServices.deleteById(req.params.id);
     res.json(result);
